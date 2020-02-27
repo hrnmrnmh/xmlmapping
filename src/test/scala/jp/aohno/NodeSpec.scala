bcc77ea15a46564cad9e0f5fd4914897b64a0d8a
aohno <a.ohno@kinotrope.co.jp>
2020-02-27 19:32:20 +0900

    add Node

--- src/test/scala/jp/aohno/NodeSpec.scala
+++ src/test/scala/jp/aohno/NodeSpec.scala
@@ -1,10 +1,9 @@
-package example
+package jp.aohno
 
-import jp.aohno.Node
 import org.scalatest._
 
 class NodeSpec extends FlatSpec with Matchers {
-  "Node" should " elem count check" in {
+  "Node make" should " elem count check" in {
     val listNode = Node.make(Node.listXml)
     listNode.root.elem.size shouldEqual 2
   }
