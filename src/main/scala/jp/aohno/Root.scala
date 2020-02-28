f1cab11fb613b9c23540fbe1d2bb814d0f997d49
aohno <a.ohno@kinotrope.co.jp>
2020-02-28 11:36:15 +0900

    一応テスト通るようになったものの、納得行かない・・・

--- src/main/scala/jp/aohno/Root.scala
+++ src/main/scala/jp/aohno/Root.scala
@@ -7,16 +7,18 @@ case class Root(node: Node)
 case class Node(elem: List[String])
 
 case class RootRef(node: NodeRef) {
-  val toRoot: Root = node.elem match {
-    case elems: List[String] =>
-      Root(Node(elems))
-    case elem: String =>
-      Root(Node(List(elem)))
-    case _ => Root(Node(Nil))
-  }
+  val toRoot: Root = Root(node.toNode)
 }
 
-case class NodeRef(elem: AnyRef)
+case class NodeRef(elem: AnyRef) {
+  val toNode: Node = elem match {
+    case elems: List[String] =>
+      Node(elems)
+    case elem: String =>
+      Node(List(elem))
+    case _ => Node(Nil)
+  }
+}
 
 case object Root {
 
@@ -27,7 +29,8 @@ case object Root {
   </node>
 
   val list2ElemXml: Elem = <node>
-    <elem>text</elem> <elem>text</elem>
+    <elem>text</elem>
+    <elem>text</elem>
   </node>
 
 }
