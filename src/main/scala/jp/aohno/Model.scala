717e856e6fe833023242f5eb1f9db487b5936578
aohno <a.ohno@kinotrope.co.jp>
2020-03-05 13:10:45 +0900

    toString -> extract[String] etc...

--- src/main/scala/jp/aohno/Model.scala
+++ src/main/scala/jp/aohno/Model.scala
@@ -3,7 +3,7 @@ package jp.aohno
 import org.json4s.native.Serialization
 import org.json4s.{CustomSerializer, DefaultFormats, Formats, JValue, Xml, _}
 
-import scala.xml.Elem
+import scala.xml.NodeSeq
 
 abstract class Model[T: Manifest] {
 
@@ -16,21 +16,23 @@ abstract class Model[T: Manifest] {
 
   class ListStringSerializer extends CustomSerializer[List[String]](_ => ( {
     case JString(x) => List(x)
-    case JArray(x) => x.map(_.toString)
+    case JArray(x) => x.map(_.extract[String])
     case JNothing => Nil
     case JNull => Nil
   }, {
     case x: List[String] => JArray(x.map(JString(_)))
-  }
+  }))
 
-  ))
-
-  def apply(xmlElement: Elem): T = {
-    val jValue = Xml.toJson(xmlElement)
+  def apply(nodeSeq: NodeSeq): T = {
+    val jValue = Xml.toJson(nodeSeq)
     apply(jValue)
   }
 
   def toJson(JValue: JValue): String = {
     Serialization.write(JValue)
+  }
+
+  def toJson(instance: T): String = {
+    Serialization.write(instance)
   }
 }
