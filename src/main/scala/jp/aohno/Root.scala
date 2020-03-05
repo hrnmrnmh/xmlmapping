9307a394a9e420b3e92cc6983583ee60322862ba
aohno <a.ohno@kinotrope.co.jp>
2020-03-05 12:53:24 +0900

    move sampleXml toSpec

--- src/main/scala/jp/aohno/Root.scala
+++ src/main/scala/jp/aohno/Root.scala
@@ -1,21 +1,7 @@
 package jp.aohno
 
-import scala.xml.Elem
-
 case class Root(node: Node)
 
 case class Node(elem: List[String])
 
-case object Root  extends Model[Root]{
-
-  val emptyElemXml: Elem = <node></node>
-
-  val singleElemXml: Elem = <node>
-    <elem>text</elem>
-  </node>
-
-  val list2ElemXml: Elem = <node>
-    <elem>text</elem>
-    <elem>text</elem>
-  </node>
-}
+case object Root  extends Model[Root]
--- src/test/scala/jp/aohno/RootSpec.scala
+++ src/test/scala/jp/aohno/RootSpec.scala
@@ -2,17 +2,30 @@ package jp.aohno
 
 import org.scalatest._
 
+import scala.xml.Elem
+
 class RootSpec extends FlatSpec with Matchers {
   "Root apply" should " elem count check" in {
-    val listRoot = Root(Root.list2ElemXml)
+    val listRoot = Root(RootSpec.list2ElemXml)
     listRoot.node.elem.size shouldEqual 2
 
-    val emptyRoot = Root(Root.emptyElemXml)
+    val emptyRoot = Root(RootSpec.emptyElemXml)
     emptyRoot.node.elem.size shouldEqual 0
 
-    // single is not List
-    // throw error
-    val singleRoot = Root(Root.singleElemXml)
+    val singleRoot = Root(RootSpec.singleElemXml)
     singleRoot.node.elem.size shouldEqual 1
   }
+}
+
+object RootSpec{
+  val emptyElemXml: Elem = <node></node>
+
+  val singleElemXml: Elem = <node>
+    <elem>text</elem>
+  </node>
+
+  val list2ElemXml: Elem = <node>
+    <elem>text</elem>
+    <elem>text</elem>
+  </node>
 }
